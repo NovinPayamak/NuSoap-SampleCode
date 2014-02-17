9f06b47e8d8881b6dcb221c71d250f0bd535ac23
Masoud A <amini141@gmail.com>
2014-02-17 12:55:43 +0330

    Update Nusoap.php

--- Nusoap.php
+++ Nusoap.php
@@ -7,13 +7,14 @@
 	$client =  new nusoap_client('http://www.novinpayamak.com/services/SMSBox/wsdl', 'wsdl'); 
 	$client->soap_defencoding = 'UTF-8';
 
-	$res = $client->call('Send', array(
-													array(
-															'Auth' 	=> array('number' => $number, 'pass' => $pass),
-															'Recipients' 		=> array($Recipients),
-															'Message' 	=> array($Message),
-															'Flash' 		=> $Flash
-														)
-													)
-	);
+		$res = $client->call('Send', array(
+			array(
+					'Auth' 	=> array('number' => '1000XXX', 'pass' => 'yourpass'),
+					'Recipients'=> array('string' => array('09XXXXXXX')),
+					'Message' 	=> array('string' => array('تست')),
+					'Flash' 	=> false
+				)
+		));
+print_r($res);
+?>
 	
